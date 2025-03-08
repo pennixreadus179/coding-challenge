925de395a8f58895a2a08ee5f5a1fa3ace7bd028
pennixreadus179 <pennixreadus179@gmail.com>
2025-03-08 06:35:25 +0000

    Add 3.php

---
+++ src/3.php
@@ -0,0 +1,15 @@
+<?php
+function getRandomNumber($min, $max) {
+    return rand($min, $max);
+}
+
+function getRandomString($length) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
+?>
